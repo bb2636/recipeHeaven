730b14a15a51c2691dc9ec571548c74cf8b3ea49
[SW7] 이수빈 <dltnqls3267@naver.com>
2023-12-15 16:08:42 +0900

    feat:usermodel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig)],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), UserModule],
 })
 export class AppModule {}
