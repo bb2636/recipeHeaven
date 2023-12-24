91bee9abe2c2f0f58739e672ce60a19ff2597688
[SW7] 이수빈 <dltnqls3267@naver.com>
2023-12-24 12:28:26 +0900

    chore:date추가

--- src/recipe/recipe.entity.ts
+++ src/recipe/recipe.entity.ts
@@ -4,10 +4,13 @@ import { Review } from 'src/review/review.entity';
 import {
   BaseEntity,
   Column,
+  CreateDateColumn,
+  DeleteDateColumn,
   Entity,
   ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 @Entity()
@@ -31,6 +34,15 @@ export class Recipe extends BaseEntity {
 
   @Column({ type: 'int' })
   level: number;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ type: 'timestamp' })
+  deletedAt: Date;
 
   @Column('json')
   ingredient: { item: string; unit: string }[]; //배열안에 객체
